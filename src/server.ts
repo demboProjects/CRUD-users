596fe48c1f0eaf44dccaa88aa41fafa65715835b
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-30 19:35:01 -0300

    inicialisa o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,65 @@
-import express from "express";
+import express, { request, response } from "express";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.use(express.json());
 
-    return response.send("Enginheiro Adilson");
+interface IUsers {
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    password: string
+}
+
+// [ {}, {}, {}]
+/**
+ * CRUD
+ * Create -> C
+ * Read -> R
+ * Update -> U
+ * Delete -> D
+ */
+
+const users: IUsers[] = [];
+
+// Read
+app.get("/users", (request, response) => {
+
+    return response.status(200).json(users);
+});
+
+// Create 
+app.post("/users", (request, response) => {
+
+    const { name, email, phone, password } = request.body;
+
+    const newUser = {
+        id: uuid(),
+        name,
+        email,
+        phone,
+        password
+    }
+
+    users.push(newUser);
+
+    return response.status(201).json(newUser);
+
 });
 
 
+// const usuario2 = {
+//     name: "Dembo",
+//     email: "josedembo@gmail.com"
+// }
+
+// const { name, email } = usuario2
+
+// console.log(name, email)
+
+
 app.listen(3333);
+
+
